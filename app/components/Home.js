e64aa123acc94bdc41dce0995949c59e065397fa
david <davidlivingrooms@gmail.com>
2017-04-26 19:31:12 -0600

    adding split panes

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,17 +1,30 @@
 // @flow
 import React, { Component } from 'react';
+import SplitPane from 'react-split-pane';
 import { Link } from 'react-router-dom';
 import styles from './Home.css';
+import './SplitPane.css';
 
 export default class Home extends Component {
   render() {
+
     return (
-      <div>
-        <div className={styles.container} data-tid="container">
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
-        </div>
-      </div>
-    );
+      <SplitPane split="vertical" defaultSize="33%">
+      <div />
+      <SplitPane split="vertical" defaultSize="50%">
+      <div />
+      <div />
+      </SplitPane>
+      </SplitPane>
+  );
+
+    // return (
+    //   <div>
+    //     <div className={styles.container} data-tid="container">
+    //       <h2>Home</h2>
+    //       <Link to="/counter">to Counter</Link>
+    //     </div>
+    //   </div>
+    // );
   }
 }
