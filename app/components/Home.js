16e8d93003b04ede85ac76b42a0b5045479e8284
david <davidlivingrooms@gmail.com>
2017-04-28 14:32:11 -0700

    markdown editor styling.

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,27 +4,49 @@ import SplitPane from 'react-split-pane';
 import { Link } from 'react-router-dom';
 import styles from './Home.css';
 import './SplitPane.css';
+import SimpleMDE from 'react-simplemde-editor'
+import './simplemde.min.global.css'
+import './editorStyles.global.css'
 
 export default class Home extends Component {
+
+
+  // constructor () {
+  //   super()
+  //   this.state = {
+  //     panelSize: 200
+  //   }
+  // }
+
+  // onClickHandler = () => {
+  //   setInterval(() => {
+  //     this.setState({panelSize: this.state.panelSize - 10})
+  //   }, 10)
+  // }
+
+  handleEditorChange = (value) => {
+    console.log(value)
+  }
+
+
   render() {
 
     return (
-      <SplitPane split="vertical" defaultSize="33%">
-      <div />
-      <SplitPane split="vertical" defaultSize="50%">
-      <div />
-      <div />
+      <SplitPane split="vertical" defaultSize={150} minSize={150}>
+        <div>
+          <p>1</p>
+        </div>
+        <SplitPane split="vertical" defaultSize={200} minSize={200}>
+          <div>
+            <p>2</p>
+          </div>
+          <div>
+            <SimpleMDE
+              onChange={this.handleEditorChange}
+            />
+          </div>
+        </SplitPane>
       </SplitPane>
-      </SplitPane>
-  );
-
-    // return (
-    //   <div>
-    //     <div className={styles.container} data-tid="container">
-    //       <h2>Home</h2>
-    //       <Link to="/counter">to Counter</Link>
-    //     </div>
-    //   </div>
-    // );
+    )
   }
 }
